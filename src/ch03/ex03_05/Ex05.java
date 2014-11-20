90741d81124c113e7f6990d0d3daee1df718bf3d
p000480075 <p000480075@A050790171.rfg.rfgad.ricoh.com>
2014-11-21 08:24:19 +0900

    commit

--- src/ch03/ex03_05/Ex05.java
+++ src/ch03/ex03_05/Ex05.java
@@ -1,15 +1,23 @@
 package ch03.ex03_05;
 
-import java.awt.Color;
 import java.util.function.UnaryOperator;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 
 public class Ex05 {
-	
+
 	public static Image transform(Image in, UnaryOperator<Color> f) {
 		int width = (int)in.getWidth();
-		
+		int height = (int)in.getWidth();
+		WritableImage out = new WritableImage(width, height);
+		for(int x =0; x < width; x++) {
+			for(int y = 0; y < height; y++) {
+				out.getPixelWriter().setColor(x, y, f.apply(in.getPixelReader().getColor(x, y)));
+			}
+		}
+		return out;
 	}
 
 }
